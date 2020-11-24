9bd08c627a60b4dfd88bad161bf85b751dddbcc3
sosinatolossa <sosinatolossa16@gmail.com>
2020-11-24 11:45:02 -0600

    did some touches

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -5,7 +5,10 @@
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--id" class="journalEntry">${entry.id}</section>
-            <section id="entry--date"  class="journalEntry">${entry.date}"</section>
+            <section id="entry--date"  class="journalEntry">${entry.date}
+                <input class="edit_botton" type="submit" value="edit">
+                <input class="delete_button" type="submit" value="delete">
+            </section>
             <section id="entry--concept" class="journalEntry">${entry.concept}</section>
             <section id="entry--description" class="journalEntry">${entry.description}</section>
             <section id="entry--mood" class="journalEntry">${entry.mood}</section>
