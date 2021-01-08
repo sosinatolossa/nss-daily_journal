c1cbd776db3ca8a1cbff7b419412f71befaee9b7
sosinatolossa <sosinatolossa16@gmail.com>
2021-01-08 13:06:49 -0600

    modified modules

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
 
+import { JournalFormComponent } from "./form/JournalForm.js"
+
 //importing the list component from JournalEntryList
 import { EntryListComponent } from "./JournalEntryList.js"
 
 import { getEntries } from "./JournalDataProvider.js"
+
+JournalFormComponent()
+
 //Invoking the the list component
 EntryListComponent()
 
